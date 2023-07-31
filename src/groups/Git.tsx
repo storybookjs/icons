e66aad0d208ee39a247873bf023e6cc581b7ece6
Charles de Dreuille <charles.dedreuille@gmail.com>
2023-07-31 14:20:08 +0200

    Remove

--- src/groups/Git.tsx
+++
@@ -1,5 +0,0 @@
-export { Repo } from './../icons/Repo';
-export { Commit } from './../icons/Commit';
-export { Branch } from './../icons/Branch';
-export { Pullrequest } from './../icons/Pullrequest';
-export { Merge } from './../icons/Merge';
