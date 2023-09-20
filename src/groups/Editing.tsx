c2b0c860cc4fff317dd5356dc259b1f7b99e96b8
Charles de Dreuille <charles.dedreuille@gmail.com>
2023-09-20 18:24:07 +0100

    Remove groups

--- src/groups/Editing.tsx
+++
@@ -1,8 +0,0 @@
-export { Markup } from './../icons/Markup';
-export { Bold } from './../icons/Bold';
-export { Italic } from './../icons/Italic';
-export { PaperClip } from './../icons/PaperClip';
-export { ListOrdered } from './../icons/ListOrdered';
-export { ListUnordered } from './../icons/ListUnordered';
-export { Paragraph } from './../icons/Paragraph';
-export { Markdown } from './../icons/Markdown';
--- src/groups/OS.tsx
+++
@@ -1,5 +0,0 @@
-export { Apple } from './../icons/Apple';
-export { Linux } from './../icons/Linux';
-export { Ubuntu } from './../icons/Ubuntu';
-export { Windows } from './../icons/Windows';
-export { Chrome } from './../icons/Chrome';
